f5cf758ac6c6a6d4335b2109db132026daac5d00
derpWanda <derpWanda@gmail.com>
2020-04-23 13:30:41 -0500

    update .send to .status

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ const server = express(); // creates the server
 
 // handle requests to the root of the api, the / route
 server.get('/', (req, res) => {
-    res.send('I\'m Alive');
+    // res.send('I\'m Alive');
+    res.status(200).json({ api: 'I\'m Alive!!!' })
 });
+
+server.get('/hello', (req, res) => {
+    // res.send({ hello: "Wanda" })
+    res.status(200).json({ hello: "Wanda" })
+})
 
 // watch for connections on port 5000
 const port = 5000
